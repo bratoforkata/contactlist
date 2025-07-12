d406ee6169673493460a792bb3f12f543e4a2e88
Hris <hris.jr@yahoo.com>
2025-07-12 15:13:34 +0100

    Clearcommand update new (#2)

---
+++ ConsoleApp1/ConsoleApp1/ClearCommand.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1;
+
+internal class ClearCommand : Command
+{
+    Dictionary<string, Command> commands = new Dictionary<string, Command>();
+
+    public ClearCommand(Dictionary<string, Command> commands) : base(0) 
+    {
+        this.commands = commands;
+    }
+    protected override void RunCommand(Queue<string> commandQueue)
+    {
+        foreach (KeyValuePair<string, Command> command in commands)
+        {
+            Console.Clear();
+            Console.WriteLine(string.Join(", ", commands.Keys));
+        }
+    }
+}
